d3e4d5a89a810403bcf3e80b5166035e87545eb1
lpl <mohamed.ali.saidina+github@gmail.com>
2017-09-04 17:37:48 +0200

    Cleanup virt-addr function

--- .bash_functions/virt-addr.sh
+++ .bash_functions/virt-addr.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Returns the IP address of a running KVM guest VM
 # Assumes a working KVM/libvirt environment
 #
@@ -10,7 +9,9 @@
 #
 virt-addr() {
     VM="$1"
-    arp -an | grep "`virsh dumpxml $VM | grep "mac address" | sed "s/.*'\(.*\)'.*/\1/g"`" | awk '{ gsub(/[\(\)]/,"",$2); print $2 }'
+    arp -an \
+        | grep "`virsh dumpxml $VM | grep "mac address" | sed "s/.*'\(.*\)'.*/\1/g"`" \
+        | awk '{ gsub(/[\(\)]/,"",$2); print $2 }'
 }
 
 # source: https://gist.github.com/mistofvongola/4447791
