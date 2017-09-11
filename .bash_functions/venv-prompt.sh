337cab677427abf86df12513c2c69e0cb44a8929
lpl <mohamed.ali.saidina+github@gmail.com>
2017-09-11 10:53:23 +0200

    Update doc on venv-prompt

--- .bash_functions/venv-prompt.sh
+++ .bash_functions/venv-prompt.sh
@@ -1,11 +1,21 @@
-# Update prompt with virtualenv
+#!/usr/bin/env bash
+# Update prompt with virtualenv.
 #
-# Usage load it in your .bashrc after defining $ORIGINAL_PROMPT instead of PS1.
+# Should work with any virtualenv tool.
+# Force prompt update with pyenv-virtualenv.
+# see: https://github.com/pyenv/pyenv-virtualenv/issues/135
+#
+# Usage:
+#   - Load it in your .bashrc after defining
+#     $ORIGINAL_PROMPT instead of PS1.
+#     Then add a line export PROMPT_COMMAND='venv-prompt'
+#     to allow Bash to executes this function just before
+#     displaying the PS1 variable.
 function venv-prompt {
 
     WANTED_PROMPT="(${VIRTUAL_ENV##*/}) $ORIGINAL_PROMPT"
     # Current virtualenv
-    if [[ $VIRTUAL_ENV != "" ]] && [[ $PS1 != $WANTED_PROMPT ]]; then
+    if [[ $VIRTUAL_ENV != "" ]] && [[ $PS1 != "$WANTED_PROMPT" ]]; then
         PROMPT=$WANTED_PROMPT
     else
         PROMPT=$ORIGINAL_PROMPT
